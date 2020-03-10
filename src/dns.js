0ad820b62951198a483f134da8361bd1f58d57bd
Daan Middendorp <github-d@landgenoot.com>
2020-03-10 23:23:17 +0100

    More refactoring

--- src/dns.js
+++ src/dns.js
@@ -2,7 +2,7 @@ const dnsPromises = require("dns").promises;
 
 /**
  * Fetch the lnd-pubkey from the DNS TXT record
- * for the given hostname
+ * for the given hostname.
  * @param {String} hostname
  * @return {Promise|String} pubkey
  */
@@ -10,10 +10,11 @@ function getLndPubkey(hostname) {
   return new Promise((resolve, reject) => {
     dnsPromises
       .resolve(hostname, "TXT")
-      .then(answers => {
-        let pubkey = answers.find(isValidPubkey);
+      .then(result => {
+        let records = result.map(answer => answer[0])
+        let pubkey = records.find(isValidPubkey);
         return pubkey
-          ? resolve(pubkey[0].substr(11, 66))
+          ? resolve(pubkey.substr(11, 66))
           : reject(new Error("No lnd-pubkey found"));
       })
       .catch(reject);
@@ -21,14 +22,14 @@ function getLndPubkey(hostname) {
 }
 
 /**
- * Check if TXT record is a valid lnd-pubkey
+ * Check if TXT record is a valid lnd-pubkey.
  * @param {String} record
  * @return {Boolean} valid
  */
 function isValidPubkey(record) {
   return (
-    record[0].substr(0, 11) === "lnd-pubkey=" &&
-    /^[0-9a-f]{66}$/.test(record[0].substr(11, 66)) // Must be hex-string with size 66
+    record.substr(0, 11) === "lnd-pubkey=" &&
+    /^[0-9a-f]{66}$/.test(record.substr(11, 66)) // Must be hex-string with size 66
   );
 }
 
