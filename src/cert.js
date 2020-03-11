5e62fbd76b21c799955f34e28a182aa54fe7d6d6
Daan Middendorp <github-d@landgenoot.com>
2020-03-11 23:05:57 +0100

    Some code formatting and comments

--- src/cert.js
+++ src/cert.js
@@ -1,13 +1,23 @@
 const rs = require("jsrsasign");
 const sslCertificate = require("get-ssl-certificate");
 
+/**
+ * Function looks up the SSL certificate for the domain, and checks if
+ * it is an OV or EV certificate by reading the following CertificatePolicies
+ * 2.23.140.1.2.2: Organization Validation
+ * 2.23.140.1.1: Extended Validation
+ * @param {String} hostname
+ * @return {Promise|Boolean}
+ */
 function isOrganization(hostname) {
   return sslCertificate.get(hostname).then(cert => {
     x509 = new rs.X509();
     x509.readCertPEM(cert.pemEncoded);
-    return x509.getExtCertificatePolicies().some(policy => {
-      return policy.id === "2.23.140.1.2.2" || policy.id === "2.23.140.1.1";
-    });
+    return x509
+      .getExtCertificatePolicies()
+      .some(
+        policy => policy.id === "2.23.140.1.2.2" || policy.id === "2.23.140.1.1"
+      );
   });
 }
 
