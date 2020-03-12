9f065e35df4e7a0e581523dfc2f4a3677121852d
Daan Middendorp <github-d@landgenoot.com>
2020-03-12 15:30:33 +0100

    Mock get-ssl-certificate

--- test/unit/hostname.test.js
+++ test/unit/hostname.test.js
@@ -1,46 +1,57 @@
 const test = require("ava");
-const sinon = require("sinon")
+const sinon = require("sinon");
 const hostname = require("../../src/lib/hostname");
 const dns = require("dns").promises;
+const sslCertificate = require("get-ssl-certificate");
+const fs = require("fs").promises;
 
-test("isOrganization valid DV cert", async t => {
-  t.is(await hostname.isOrganization("publisher.landgenoot.com"), false);
+test.serial("isOrganization valid DV cert", async t => {
+  let dvCert = await fs.readFile("test/data/dv-cert.pem")
+  sinon.stub(sslCertificate, "get").resolves({
+    pemEncoded: dvCert.toString()
+  });
+  t.is(await hostname.isOrganization("domain-with-dv-cert.com"), false);
+  sslCertificate.get.restore();
 });
 
-test("isOrganization valid OV cert", async t => {
-  t.is(await hostname.isOrganization("spiegel.de"), true);
+test.serial("isOrganization valid OV cert", async t => {
+  let dvCert = await fs.readFile("test/data/ov-cert.pem")
+  sinon.stub(sslCertificate, "get").resolves({
+    pemEncoded: dvCert.toString()
+  });
+  t.is(await hostname.isOrganization("domain-with-ov-cert.com"), true);
+  sslCertificate.get.restore();
 });
 
-test("isOrganization valid EV cert", async t => {
-  t.is(await hostname.isOrganization("paypal.com"), true);
-});
-
-test("isOrganization not existing hostname", async t => {
-  const error = await t.throwsAsync(
-    async () => {
-      await hostname.isOrganization("non.existing.tld");
-    },
-    { instanceOf: Error }
-  );
-  t.is(error.code, "ENOTFOUND");
+test.serial("isOrganization valid EV cert", async t => {
+  let dvCert = await fs.readFile("test/data/ev-cert.pem")
+  sinon.stub(sslCertificate, "get").resolves({
+    pemEncoded: dvCert.toString()
+  });
+  t.is(await hostname.isOrganization("domain-with-ev-cert.com"), true);
+  sslCertificate.get.restore();
 });
 
 test.serial("lnd-pubkey valid example", async t => {
-  sinon.stub(dns, "resolve").resolves([
-    ["lnd-pubkey=027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"]
-  ]);
+  sinon
+    .stub(dns, "resolve")
+    .resolves([
+      [
+        "lnd-pubkey=027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"
+      ]
+    ]);
   let pubkey = await hostname.getLndPubkey("publisher.landgenoot.com");
   t.is(
     pubkey,
     "027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"
   );
-  dns.resolve.restore()
+  dns.resolve.restore();
 });
 
 test.serial("lnd-pubkey invalid example", async t => {
-  sinon.stub(dns, "resolve").resolves([
-    ["docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"]
-  ]);
+  sinon
+    .stub(dns, "resolve")
+    .resolves([["docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"]]);
   const error = await t.throwsAsync(
     async () => {
       await hostname.getLndPubkey("example.com");
@@ -48,6 +59,5 @@ test.serial("lnd-pubkey invalid example", async t => {
     { instanceOf: Error }
   );
   t.is(error.message, "No lnd-pubkey found");
-  dns.resolve.restore()
+  dns.resolve.restore();
 });
-
