32c8d9fe370975ba629a03274bc4f6dee6b75e72
Daan Middendorp <github-d@landgenoot.com>
2020-03-12 15:55:56 +0100

    Move from ava to mocha

--- test/unit/hostname.test.js
+++ test/unit/hostname.test.js
@@ -1,63 +1,64 @@
-const test = require("ava");
+
+const assert = require('assert');
 const sinon = require("sinon");
 const hostname = require("../../src/lib/hostname");
-const dns = require("dns").promises;
 const sslCertificate = require("get-ssl-certificate");
 const fs = require("fs").promises;
+const dns = require("dns").promises;
 
-test.serial("isOrganization valid DV cert", async t => {
-  let dvCert = await fs.readFile("test/data/dv-cert.pem")
-  sinon.stub(sslCertificate, "get").resolves({
-    pemEncoded: dvCert.toString()
+describe('hostname', () => {
+  describe('isOrganization', () => {
+    it('should return false if DV certificate', async () => {
+      let dvCert = await fs.readFile("test/data/dv-cert.pem")
+      sinon.stub(sslCertificate, "get").resolves({
+        pemEncoded: dvCert.toString()
+      });
+      assert.equal(await hostname.isOrganization("domain-with-dv-cert.com"), false);
+      sslCertificate.get.restore();
+    });
+
+    it('should return true if OV certificate', async () => {
+      let dvCert = await fs.readFile("test/data/ov-cert.pem")
+      sinon.stub(sslCertificate, "get").resolves({
+        pemEncoded: dvCert.toString()
+      });
+      assert.equal(await hostname.isOrganization("domain-with-ov-cert.com"), true);
+      sslCertificate.get.restore();
+    });
+
+    it('should return true if EV certificate', async () => {
+      let dvCert = await fs.readFile("test/data/ev-cert.pem")
+      sinon.stub(sslCertificate, "get").resolves({
+        pemEncoded: dvCert.toString()
+      });
+      assert.equal(await hostname.isOrganization("domain-with-ev-cert.com"), true);
+      sslCertificate.get.restore();
+    });
   });
-  t.is(await hostname.isOrganization("domain-with-dv-cert.com"), false);
-  sslCertificate.get.restore();
-});
 
-test.serial("isOrganization valid OV cert", async t => {
-  let dvCert = await fs.readFile("test/data/ov-cert.pem")
-  sinon.stub(sslCertificate, "get").resolves({
-    pemEncoded: dvCert.toString()
-  });
-  t.is(await hostname.isOrganization("domain-with-ov-cert.com"), true);
-  sslCertificate.get.restore();
-});
+  describe('getLndPubkey', () => {
+    it('should return pubkey if TXT record exists', async () => {
+      sinon
+        .stub(dns, "resolve")
+        .resolves([
+          [
+            "lnd-pubkey=027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"
+          ]
+        ]);
+      let pubkey = await hostname.getLndPubkey("publisher.landgenoot.com");
+      assert.equal(
+        pubkey,
+        "027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"
+      );
+      dns.resolve.restore();
+    });
 
-test.serial("isOrganization valid EV cert", async t => {
-  let dvCert = await fs.readFile("test/data/ev-cert.pem")
-  sinon.stub(sslCertificate, "get").resolves({
-    pemEncoded: dvCert.toString()
-  });
-  t.is(await hostname.isOrganization("domain-with-ev-cert.com"), true);
-  sslCertificate.get.restore();
-});
-
-test.serial("lnd-pubkey valid example", async t => {
-  sinon
-    .stub(dns, "resolve")
-    .resolves([
-      [
-        "lnd-pubkey=027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"
-      ]
-    ]);
-  let pubkey = await hostname.getLndPubkey("publisher.landgenoot.com");
-  t.is(
-    pubkey,
-    "027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9"
-  );
-  dns.resolve.restore();
-});
-
-test.serial("lnd-pubkey invalid example", async t => {
-  sinon
-    .stub(dns, "resolve")
-    .resolves([["docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"]]);
-  const error = await t.throwsAsync(
-    async () => {
-      await hostname.getLndPubkey("example.com");
-    },
-    { instanceOf: Error }
-  );
-  t.is(error.message, "No lnd-pubkey found");
-  dns.resolve.restore();
+    it('should throw error if TXT record does not exist', async () => {
+      sinon
+        .stub(dns, "resolve")
+        .resolves([["docusign=1b0a6754-49b1-4db5-8540-d2c12664b289"]]);
+      assert.rejects(() => hostname.getLndPubkey("example.com"), Error, "Error thrown");
+      dns.resolve.restore();
+    });
+  })
 });
--- test/unit/lnd.test.js
+++ test/unit/lnd.test.js
@@ -1,16 +1,16 @@
-const test = require("ava");
-const lnd = require("../../src/lib/lnd")
+// const test = require("ava");
+// const lnd = require("../../src/lib/lnd")
 
-test.skip("Open macaroon", async t => {
-  let macaroon = await lnd.readMacaroon("/home/daan/.lnd/data/chain/bitcoin/mainnet/admin.macaroon")
-  t.regex(macaroon, /[0-9a-f]/);
-});
+// test.skip("Open macaroon", async t => {
+//   let macaroon = await lnd.readMacaroon("/home/daan/.lnd/data/chain/bitcoin/mainnet/admin.macaroon")
+//   t.regex(macaroon, /[0-9a-f]/);
+// });
 
-test.skip("Keysend request", async t => {
-  let request = lnd.keysendRequest("027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9", 10);
-  t.is(request.dest_string, "027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9")
-  t.is(request.amt, 10);
-  t.is(request.final_cltv_delta, 40);
-  t.regex(request.payment_hash, /[0-9a-f]/);
-  t.not(request.dest_custom_records[5482373484], undefined)
-})
+// test.skip("Keysend request", async t => {
+//   let request = lnd.keysendRequest("027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9", 10);
+//   t.is(request.dest_string, "027d2456f6d4aaf27873b68b7717c8137aaa8043d687a2113b916a5016e9a880e9")
+//   t.is(request.amt, 10);
+//   t.is(request.final_cltv_delta, 40);
+//   t.regex(request.payment_hash, /[0-9a-f]/);
+//   t.not(request.dest_custom_records[5482373484], undefined)
+// })
