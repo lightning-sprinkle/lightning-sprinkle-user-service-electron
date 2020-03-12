370ca9651c821e93892d9dcdb1f7533ac4cbf5e8
Daan Middendorp <github-d@landgenoot.com>
2020-03-12 19:58:41 +0100

    Make tray fancy

--- src/main.js
+++ src/main.js
@@ -4,25 +4,41 @@ const { app, Tray, Menu, BrowserWindow } = require("electron");
 const api = require("./routes");
 const lndChildProcess = require("./lnd-child-process");
 
-
 const Logger = {
   info: msg => {
     console.log(msg);
   },
   error: msg => {
     console.error(msg);
-  },
+  }
 };
 
-lndChildProcess.startLndProcess("/home/daan/.config/lightning-app/lnd", Logger)
+// lndChildProcess.startLndProcess("/home/daan/lndsettings", Logger)
 
 let top = {};
 
 app.once("ready", ev => {
-  api.init();
   top.tray = new Tray("icon.png");
-  const menu = Menu.buildFromTemplate([{ role: "quit" }]);
-  top.tray.setToolTip("hello electrol");
+  const menu = Menu.buildFromTemplate([
+    {
+      label: "Lightning Sprinkle 0.1",
+      enabled: false
+    },
+    {
+      label: "Running",
+      enabled: false
+    },
+    { type: "separator" },
+    {
+      label: "Manage",
+      click: function() {
+        console.log("Clicked on settings");
+      }
+    },
+    {
+      role: "quit"
+    }
+  ]);
   top.tray.setContextMenu(menu);
 });
 app.on("before-quit", ev => {
